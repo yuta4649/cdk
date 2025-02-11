5bc20754cb340a0b85d7a9a3cc400d8f8813c68e
森雄太 <yutamori@moriyuutanoMac-mini.local>
2025-02-11 21:25:10 +0900

    mod fsx

--- src/constructs/fsx.ts
+++ src/constructs/fsx.ts
@@ -1,6 +1,14 @@
+import * as fsx from 'aws-cdk-lib/aws-fsx';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
+export interface FsxProps {
+    vpc: ec2.Vpc;
+}
+
 export class Fsx extends Construct {
-  
+  fsx: fsx.CfnFileSystem;
+  constructor(scope: Construct, id: string, props: FsxProps) {
+    super(scope, id);
+  }
 }
